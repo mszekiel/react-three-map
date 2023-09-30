1f69ceab6d5f7a3a04b3f97115dd02a2eba82bfb
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2023-09-30 14:58:15 +0200

    feat: export r3m store

--- src/mapbox.index.ts
+++ src/mapbox.index.ts
@@ -1,8 +1,11 @@
 import type { Map } from 'mapbox-gl';
 import { useMap as useMapGeneric } from './api/use-map';
+import { useR3M } from './core/store';
+
 
 export * from './api/canvas-props';
 export * from './api/coordinates';
 export * from './mapbox/canvas';
 
 export const useMap = useMapGeneric<Map>;
+export const useThreeMap = useR3M
--- src/maplibre.index.ts
+++ src/maplibre.index.ts
@@ -1,8 +1,10 @@
 import type { Map } from 'maplibre-gl';
 import { useMap as useMapGeneric } from './api/use-map';
+import { useR3M } from './core/store';
 
 export * from './api/canvas-props';
 export * from './api/coordinates';
 export * from './maplibre/canvas';
 
 export const useMap = useMapGeneric<Map>;
+export const useThreeMap = useR3M
